765c9e3bdfb1e3f821a84cd21f929c7d7901d66c
Seth Bruner <bruner.seth@outlook.com>
2024-02-09 17:09:28 -0500

    new directory, new solution files for OOP

--- Lesson3/builtInConstructos.js
+++ Lesson3/builtInConstructos.js
@@ -11,3 +11,16 @@ console.log(Object.getPrototypeOf(Array.prototype) === Object.prototype);
 let sethBirthDay = new Date('December 28 1990 11:37 am');
 console.log(sethBirthDay.getMonth());
 console.log(sethBirthDay.getFullYear());
+//builtIn_feature
+console.log(sethBirthDay.toDateString());
+
+let stringObject = new String('Hello');
+console.log(stringObject);
+console.log(typeof stringObject);
+
+Array.prototype.first = function() {
+  return this[0];
+};
+
+console.log(newArray.first());
+
