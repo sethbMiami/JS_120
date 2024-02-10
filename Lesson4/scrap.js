98ca708f7903cac2bf41799a89cca88c7b72fdbd
Seth Bruner <bruner.seth@outlook.com>
2024-02-10 18:29:25 -0500

    polymorphism oop practice

--- Lesson4/scrap.js
+++ Lesson4/scrap.js
@@ -31,3 +31,53 @@ sqr.toString();    // => "[Square 5 x 5]"
 
 console.log(Square.prototype.constructor === Rectangle);
 console.log(Square.prototype.constructor === Square);
+console.log();
+
+class Animal {}
+class Cat extends Animal {}
+let fluffy = new Cat();
+console.log(fluffy instanceof Animal);
+
+function Person(name) {
+  this.name = name;
+  this.school = undefined;
+}
+
+Person.prototype.speak = function() {
+  return `Hello, my name is ${this.name}.`;
+};
+
+// missing code
+function Child(name, school) {
+  Person.call(this, name);
+  this.school = school;
+}
+//more missing code
+Child.prototype = Object.create(Person.prototype);
+Child.prototype.constructor = Child;
+// Child.prototype = Object.assign(Person.prototype);
+// Child.prototype["constructor"] = Child;
+
+
+Child.prototype.learn = function() {
+  return "I'm going to school!";
+};
+
+let child = new Child("Suzy", "PS 33");
+console.log(child instanceof Child);                               // true
+console.log(child instanceof Person);                              // true
+console.log(Object.getPrototypeOf(child) === Child.prototype);     // true
+console.log(Object.getPrototypeOf(child).constructor === Child);   // true
+console.log(child.school === "PS 33");                             // true
+console.log(child.learn() === "I'm going to school!");             // true
+console.log(child.speak() === "Hello, my name is Suzy.");          // true
+console.log();
+
+let person = new Person("Pete");
+console.log(person instanceof Child === false);                    // true
+console.log(person instanceof Person);                             // true
+console.log(Object.getPrototypeOf(person) === Person.prototype);   // true
+console.log(Object.getPrototypeOf(person).constructor === Person); // true
+console.log(person.school === undefined);                          // true
+console.log(person.speak() === "Hello, my name is Pete.");         // true
+console.log(person.learn === undefined);                           // true
